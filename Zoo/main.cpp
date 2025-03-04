818b1fa9e9b314886a325fe306b65bf883af5272
hwilliford <hwilliford@pima.edu>
2025-03-03 17:51:11 -0700

    Update main.cpp
    update for lecture on Mar032025

--- Zoo/main.cpp
+++ Zoo/main.cpp
@@ -1,284 +1,199 @@
+#include <list>
+using std::list;
+
+#include <vector>
+using std::vector;
+
+#include <deque>
+using std::deque;
+
+#include <stack>
+using std::stack;
+
+#include <queue>
+using std::queue;
+#include <iomanip>
+#include <map>
+using std::map;
+using std::pair;
+
 #include <iostream>
-#include <string>
-#include <format>
 
-#define ANIMAL_HEADER
-#define ENABLE_DEBUG		// comment out to disable debugging
+#include "Animal.h"
+#include "Cow.h"
 
-#ifdef ENABLE_DEBUG
-const std::string debugHeader = "info : \t";
-#endif
+void testLinkedList() {
+	list<Cow>	cowList;	// create a list of cows.
 
+	// Get some cows, name them and put them into a list.
+	Cow* c0 = new Cow();	// make a new cow in dynamic memory
+	c0->SetName("Mr. Moo");	// use member function to SetName of cow c0.
 
-using namespace std;
+	cowList.push_back(*c0);	// add the cow to the heardList.
 
-class Animal	// base class
-{
-private:
-	std::string name;
-	std::string title;
-	std::string greeting;
-	std::string animalLicense;
-	static int numAnimals;				// initialization for static in is done at end of file.
+	// The next 4 instances are anonymous objects in main()
+	// There is no way to access them outside of the heardList.
+	cowList.push_back(Cow("Bessie"));
+	cowList.push_back(Cow("Daisy"));
+	cowList.push_back(Cow("Buttercup"));
+	cowList.push_back(Cow("Mooana"));
 
-	enum BodyStructure { undifined_b, verterbrate, inverterbrate };
-	enum Habitat { undifined_h, terrestrial, aquatic, aeial };
-	enum Diet { undifined_d, herbivore, carnivore, omnivore, detritivore };
-	enum Locomotion { undifined_l, walking, flying, swimming, crawling };
-	enum Respiration { undifined_r, lungs, gills };
-
-	BodyStructure bodyStructure{ undifined_b };
-	Habitat habitat{ undifined_h };
-	Diet diet{ undifined_d };
-	Locomotion locomotion{ undifined_l };
-	Respiration respiration{ undifined_r };
-
-public:
-	//	constructors / destructor (virtural)
-	//	public access functions, public interface &c.
-
-
-	Animal();
-	Animal(const Animal&);							// copy construtor
-	Animal(const std::string&, const std::string&);
-	//	Animal(const std::string&, const std::string&, const Animal::BodyStructure);
-	~Animal();	// destructor
-	void Print() const;
-
-	const std::string GetName() const { return name; }
-	const std::string GetTitle() const { return title; }
-	const std::string GetGreeting() const { return greeting; }
-	const std::string GetLicense() const { return animalLicense; }
-
-	const std::string GetBodyStructure() const {
-		switch (bodyStructure) {
-		case verterbrate:
-			return "verterbrate";
-			break;
-		case inverterbrate:
-			return "inverterbrate";
-			break;
-		case undifined_b:
-			return "undifined_b";
-			break;
-		default:
-			return "should not have got here: GetBodyStructure();";
-		}
+	// now lets walk the list 
+	while (!cowList.empty()) {
+		cowList.front().Print();
+		cowList.pop_front();
 	}
-
-	const std::string GetHabitat() const {
-		switch (habitat) {
-		case terrestrial:
-			return "terrestrial";
-			break;
-		case aquatic:
-			return "aquatic";
-			break;
-		case aeial:
-			return "aeial";
-			break;
-		case undifined_h:
-			return "undifined_h";
-			break;
-		default:
-			return "should not have got here: Gethabitat();";
-		}
-	}
-
-	const std::string GetDiet() const {
-		switch (diet) {
-		case herbivore:
-			return "herbivore";
-			break;
-		case carnivore:
-			return "carnivore";
-			break;
-		case omnivore:
-			return "omnivore";
-			break;
-		case detritivore:
-			return "detritivore";
-			break;
-		case undifined_d:
-			return "undifined_d";
-			break;
-		default:
-			return "should not have got here: GetDiet();";
-
-		}
-	}
-
-	const std::string GetLocomotion() const {
-		switch (locomotion) {
-		case walking:
-			return "walking";
-			break;
-		case flying:
-			return "flying";
-			break;
-		case swimming:
-			return "swimming";
-			break;
-		case crawling:
-			return "crawling";
-			break;
-		case undifined_l:
-			return "undifined_l";
-			break;
-		default:
-			return "should not have got here: GetLocomotion();";
-		}
-	}
-
-	const std::string GetRespiration() const {
-		switch (respiration) {
-		case lungs:
-			return "lungs";
-			break;
-		case gills:
-			return "gills";
-			break;
-		case undifined_r:
-			return "undifined_r";
-			break;
-		default:
-			return "should not have got here: GetRespiration()";
-
-		}
-	}
-
-	void SetBodyStructureVerterbrate() { bodyStructure = verterbrate; }
-	void SetBodyStructureInverterbreate() { bodyStructure = inverterbrate; }
-	void SetHabitatTerestrial() { habitat = terrestrial; }
-	void SetHabitatAquatic() { habitat = aquatic; }
-	void SetHabitatAeial() { habitat = aeial; }
-	void SetDietHerbivore() { diet = herbivore; }
-	void SetDietCarnivore() { diet = carnivore; }
-	void SetDietOmnivore() { diet = omnivore; }
-	void SetDietDetritivore() { diet = detritivore; }
-	void SetLocomotionWalking() { locomotion = walking; }
-	void SetLocomotionFlying() { locomotion = flying; }
-	void SetLocomotionSwimming() { locomotion = swimming; }
-	void SetLocomotionCrawling() { locomotion = crawling; }
-	void SetRespirationLungs() { respiration = lungs; }
-	void SetRespirationGills() { respiration = gills; }
-	void SetName(const std::string& s) { name = s; }
-	void SetTitle(const std::string& t) { title = t; }
-	void SetGreeting(const std::string& g) { greeting = g; }
-	void SetLicense(size_t length) {
-		const std::string characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-		std::string rs;
-
-		for (size_t i = 0; i < length; ++i) 
-			rs += characters[rand() % characters.length()];
-
-		animalLicense = ISA() + "-" + rs;
-	}
-
-	static int GetNumberAnimals() { return numAnimals; }
-
-	std::string ISA() { return "animal"; }
-};
-
-void Animal::Print() const
-{
-	std::cout << std::format("{:05} {:>10} {:<30} {:>15} {:>20p}\n",
-		GetNumberAnimals(), GetTitle(), GetName(), GetLicense(), (void*)this);
-	std::cout << std::format("\tGreeting       :{:>15}\n", GetGreeting());
-	std::cout << std::format("\tBody Structure :{:>15}\n", GetBodyStructure());
-	std::cout << std::format("\tHabitat        :{:>15}\n", GetHabitat());
-	std::cout << std::format("\tDiet           :{:>15}\n", GetDiet());
-	std::cout << std::format("\tLocomotion     :{:>15}\n", GetLocomotion());
-	std::cout << std::format("\tRespiration    :{:>15}\n", GetRespiration());
+	delete c0;	// was allocated on the heap clean up the heap
 }
 
-Animal::Animal() {
-	//	default construtor.
-	//	all strings are constructed with an empty value by default.
+void testIterator() {
+	list<Cow> cowList;
 
-	SetLicense(5);
-//	animalLicense = "TODO: implement";
-	name = "john doe";
-	title = "mr.";
-	greeting = "unknown";
+	Cow* c0 = new Cow();	// make a new cow in dynamic memory
+	c0->SetName("Clover");	// use member function to SetName of cow c0
 
-	numAnimals++;
-#ifdef ENABLE_DEBUG
-	std::cerr << debugHeader << format("{:30} {:p}.\n", "Default Animal created", (void*)this);
-#endif
+	cowList.push_back(*c0);	// add cow to cowList.
+
+	// next Cow instances are anonymous objects
+	cowList.push_back(Cow("Mabel"));
+	cowList.push_back(Cow("Rosie"));
+	cowList.push_back(Cow("Sir Loin"));
+
+	// now sort the cows by weight in Lbs.
+	cowList.sort();		//  sort() relys on the operator<
+
+	// now walk the list using simplify iterator notiation 'auto'
+	auto autoIter = cowList.begin();
+	while (autoIter != cowList.end()) {
+		(*autoIter).Print();
+		++autoIter;
+	}
 }
 
-// inline 
-Animal::Animal(const Animal& a)	// copy constructor
-{
-	bodyStructure = a.bodyStructure;
-	habitat = a.habitat;
-	diet = a.diet;
-	locomotion = a.locomotion;
-	respiration = a.respiration;
-	animalLicense = a.animalLicense;
-	this->name = a.name;
-	this->title = a.title;
-	this->greeting = a.greeting;
+void testVector() {
+	vector<Cow> cowVector1, cowVector2;
 
-	numAnimals++;
-#ifdef ENABLE_DEBUG
-	std::cerr << debugHeader << format( "{:30} {:p}.\n", "Animal copied", (void*)this);
-#endif
+	// add 3 cows as anonymous objects 
+	cowVector1.push_back(Cow("Bella"));
+	cowVector1.push_back(Cow("Penelope"));
+	cowVector1.push_back(Cow("Tulip"));
+	cowVector1.push_back(Cow("Ferdinand"));
+
+	// walk vector and print out what it has.
+	for (auto iter = cowVector1.begin(); iter != cowVector1.end(); iter++)
+		(iter)->Print();
+	
+	// walk vector in old school for loop
+	for (int i = 0; i < cowVector1.size(); i++)
+		cowVector1[i].Print();	// print first vector
+
+	cowVector2 = cowVector1;	// assign cowVectro2 to cowVector1.
+
+	// verify cowVector2 after overide=.
+	for (int i = 0; i < cowVector2.size(); i++)
+		cowVector2[i].Print();
+
+	// preferred range 'for loop' with auto, simplifying things
+	for (const auto& cow : cowVector2)
+		cow.Print();
 }
 
-Animal::Animal(const std::string& n, const std::string& t) {
+void testDeque() {
+	deque<Cow> cowDeque;		// pronounced deck
+	Cow *c0 = new Cow("Butterbean");
 
-	SetLicense(5);
-//	animalLicense = "TODO: implement";
+	// create some anonymous cow objects.
+	cowDeque.push_back(Cow("Magnolia"));
+	cowDeque.push_back(Cow("Cheddar"));
+	cowDeque.push_back(Cow("Blossom"));
 
-	name = n;
-	title = t;
-	greeting = "unknown";
+	// insert one past the beginning
+	cowDeque.insert(std::next(cowDeque.begin()), Cow("Marigold"));
 
-	numAnimals++;
-#ifdef ENABLE_DEBUG
-	std::cerr << debugHeader << format("{:30} {:p}.\n", "Animal created by name and title", (void*)this);
-#endif
+	cowDeque[1] = Cow("Cocoa");	// replace the 0th element (no bounds checking)
+
+	while (!cowDeque.empty()) {
+		cowDeque.front().Print();
+		cowDeque.pop_front();
+	}
 }
 
-Animal::~Animal() {
-	numAnimals--;
-#ifdef ENABLE_DEBUG
-	std::cerr << debugHeader << std::format("{:30} {:p}.\n", "Destructor called for", (void*)this);
-#endif
+void testStack() {
+	stack<Cow> cowStack;	// create a stack
+
+	// add Cows to the stack (anonymous objects)
+	cowStack.push(Cow("Ginger"));
+	cowStack.push(Cow("Maple"));
+	cowStack.push(Cow("Milky Way"));
+	cowStack.push(Cow("Bo Vine"));
+
+	while (!cowStack.empty()) {
+		cowStack.top().Print();
+		cowStack.pop();
+	}
 }
 
-// initialize the number of Animals. 
-int Animal::numAnimals = 0;
+void testQueue() {
+	queue<Cow> cowQueue;	// create a queue of cows
 
-void testAnimal() {
-	Animal* a0 = new Animal();				// make john doe.
-	a0->Print();
+	// add cows as anonymous objects
+	cowQueue.push(Cow("Hazel"));
+	cowQueue.push(Cow("Moochelle"));
+	cowQueue.push(Cow("Dandelion"));
+	cowQueue.push(Cow("Oreo"));
 
-	Animal* a1 = new Animal("Lily", "Miss");
-	a1->Print();
+	while (!cowQueue.empty()) {
+		cowQueue.front().Print();
+		cowQueue.pop();
+	}
+}
 
-	Animal* a2 = new Animal("Bessie", "Miss");
-	a2->Print();
+void testMap() {
+	Cow c0 ("Tilly");
+	Cow c1 ("Juniper");
+	Cow c2 ("MooMoo");
+	Cow c3 ("Sundae");
 
-	Animal* a3 = new Animal(*a0);		// make a copy of a0 into a3.
-	a3->Print();
+	// create pairings of licence to Cows
+	pair<string, Cow> cowPair0(c0.GetLicense(), c0);
+	pair<string, Cow> cowPair1(c1.GetLicense(), c1);
+	pair<string, Cow> cowPair2(c2.GetLicense(), c2);
 
-	a3->SetName("Brownie");				// modify a3
-	a3->SetTitle("Sr.");
-	a3->SetBodyStructureVerterbrate();
-	a3->SetHabitatTerestrial();
-	a3->SetDietHerbivore();
-	a3->SetLocomotionWalking();
-	a3->SetRespirationLungs();
+	// Create map of cows w/ string keys
+	map<string, Cow> cowMap;
+	cowMap.insert(cowPair0);
+	cowMap.insert(cowPair1);
+	cowMap.insert(cowPair2);
 
-	a3->Print();						// print modifyed a3
+	// insert using virtual indices per map
+	cowMap[c3.GetLicense()] = c3;
 
-	delete a1;
-	delete a2;
-	delete a3;
+	// iterate through set with map iterator
+	map<string, Cow>::iterator mapIter = cowMap.begin();
+//	mapIter = cowMap.begin();
+	while (mapIter != cowMap.end()) {
+		pair<string, Cow> temp = *mapIter;
+		Cow& tempS = temp.second;			// get the 2nd element.
+		tempS.Print();						// print it
 
-	a0->Print();
-	// delete a0;
+		++mapIter;
+	}
+
+	// lets iterate using a range-for loop with 'auto'
+	// Output should be by order of animalLicense
+	for (auto& [animalLicense, cow] : cowMap)
+		cow.Print();
+}
+
+int main() {
+
+	testLinkedList();
+	testIterator();
+	testVector();
+	testDeque();
+	testStack();
+	testQueue();
+	testMap();
+
+	return 0;
 }
